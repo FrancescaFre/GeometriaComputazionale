d98ee0d436459adcf63dc092ef4e0248408927c4
FrancescaFRE <bassi.francesca95@gmail.com>
2020-08-07 17:07:09 +0100

    add regions

--- project921900/Assets/raymarchcamera.cs
+++ project921900/Assets/raymarchcamera.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 
 public class raymarchcamera : SceneViewFilter
 {
-
+    #region material shader
     //------ Material component
     [SerializeField]
     private Shader _shader;
@@ -40,10 +40,62 @@ public class raymarchcamera : SceneViewFilter
     }
 
     private Camera _cam;
+    #endregion
+    #region push coords
+    private void PushCoords(RenderTexture source, RenderTexture destination) {
+
+        RenderTexture.active = destination;
+        GL.PushMatrix();
+        GL.LoadOrtho();
+        _raymarchMaterial.SetPass(0);
+        GL.Begin(GL.QUADS);
+
+        //BL
+        GL.MultiTexCoord2(0, 0.0f, 0.0f);
+        GL.Vertex3(0.0f, 0.0f, 3.0f);
+        //BR
+        GL.MultiTexCoord2(0, 1.0f, 0.0f);
+        GL.Vertex3(1.0f, 0.0f, 2.0f);
+        //TR
+        GL.MultiTexCoord2(0, 1.0f, 1.0f);
+        GL.Vertex3(1.0f, 1.0f, 1.0f);
+        //TL
+        GL.MultiTexCoord2(0, 0.0f, 1.0f);
+        GL.Vertex3(0.0f, 1.0f, 0.0f);
+
+        GL.End();
+        GL.PopMatrix();
+    }
+    #endregion
+    #region frustum
+    //------ Frustum 
+    private Matrix4x4 CamFrustum(Camera cam)
+    {
+
+        Matrix4x4 frustum = Matrix4x4.identity;
+        float fov = Mathf.Tan((cam.fieldOfView * 0.5f) * Mathf.Deg2Rad);
+
+        Vector3 goUp = Vector3.up * fov;
+        Vector3 goRight = Vector3.right * fov * cam.aspect;
+
+        Vector3 TL = (-Vector3.forward - goRight + goUp);
+        Vector3 TR = (-Vector3.forward + goRight + goUp);
+        Vector3 BR = (-Vector3.forward + goRight - goUp);
+        Vector3 BL = (-Vector3.forward - goRight - goUp);
+
+        frustum.SetRow(0, TL);
+        frustum.SetRow(1, TR);
+        frustum.SetRow(2, BR);
+        frustum.SetRow(3, BL);
+
+        return frustum;
+    }
+    #endregion
+
     public float _maxDistance;
 
     public Vector4 _sphere1;
-
+    public float genene;
 
     public Vector4 _box1;
     public Transform _directionalLight;
@@ -68,49 +120,8 @@ public class raymarchcamera : SceneViewFilter
         _raymarchMaterial.SetVector("_modInterval", _modInterval);
         _raymarchMaterial.SetTexture("_MainTex", source);
 
-        RenderTexture.active = destination;
-        GL.PushMatrix();
-        GL.LoadOrtho();
-        _raymarchMaterial.SetPass(0);
-        GL.Begin(GL.QUADS);
-
-        //BL
-        GL.MultiTexCoord2(0, 0.0f, 0.0f);
-        GL.Vertex3(0.0f, 0.0f, 3.0f);
-        //BR
-        GL.MultiTexCoord2(0, 1.0f, 0.0f);
-        GL.Vertex3(1.0f, 0.0f, 2.0f);
-        //TR
-        GL.MultiTexCoord2(0, 1.0f, 1.0f);
-        GL.Vertex3(1.0f, 1.0f, 1.0f);
-        //TL
-        GL.MultiTexCoord2(0, 0.0f, 1.0f);
-        GL.Vertex3(0.0f, 1.0f, 0.0f);
-
-        GL.End();
-        GL.PopMatrix();
+        PushCoords(source, destination);
     }
 
-    //------ Frustum 
-    private Matrix4x4 CamFrustum(Camera cam)
-    {
 
-        Matrix4x4 frustum = Matrix4x4.identity;
-        float fov = Mathf.Tan((cam.fieldOfView * 0.5f) * Mathf.Deg2Rad);
-
-        Vector3 goUp = Vector3.up * fov;
-        Vector3 goRight = Vector3.right * fov * cam.aspect;
-
-        Vector3 TL = (-Vector3.forward - goRight + goUp);
-        Vector3 TR = (-Vector3.forward + goRight + goUp);
-        Vector3 BR = (-Vector3.forward + goRight - goUp);
-        Vector3 BL = (-Vector3.forward - goRight - goUp);
-
-        frustum.SetRow(0, TL);
-        frustum.SetRow(1, TR);
-        frustum.SetRow(2, BR);
-        frustum.SetRow(3, BL);
-
-        return frustum;
-    }
 }
