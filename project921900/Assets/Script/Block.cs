4a809977f50042ae3081050ea27003da378931cb
FrancescaFRE <bassi.francesca95@gmail.com>
2020-08-12 20:28:51 +0100

    [REFACTORING] FIle [FIXED] subtraction [ADD] abbozzata la gui, aggiunto il drag and drop
    todo: finire la gui pls

---
+++ project921900/Assets/Script/Block.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    enum Shape {SPHERE, CUBE, ROUNDEDCUBE, TORUS}
+    enum Operator {INTERSECTION, UNION, SUBTRACTION}
+    
+    public int shape;
+    public int op;
+    public int selected;
+
+    public float size;
+    public int auto;
+    public int morph; //morph with value for the new shape
+
+
+    public Vector3 position;
+    public Vector4 rotation; //bool sugli assi e il quarto valore è il grado di rotazione
+    public Color color;
+
+    
+    public void Awake() {
+        shape = Random.Range(1, 4);
+        op = 0;
+        selected = 0;
+        size = Random.Range(0.5f, 2);
+        transform.position = new Vector3(Random.Range(-4, 4), Random.Range(-4, 4), 0); 
+    }
+
+    public void Update()
+    {
+        position = transform.position;
+    }
+
+}
---
+++ project921900/Assets/Script/drag_and_drop.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//https://www.youtube.com/watch?v=0yHBDZHLRbQ
+public class drag_and_drop : MonoBehaviour
+{
+    private Vector3 mOffset;
+    private float mZCoord;
+    Block block; 
+    private void Awake()
+    {
+         block = transform.GetComponent<Block>(); 
+    }
+    private void OnMouseDown()
+    {
+        mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z; 
+        mOffset = gameObject.transform.position - GetMouseWorldPos();
+        block.selected = 1;
+    }
+
+    private void OnMouseDrag()
+    {
+        transform.position = GetMouseWorldPos() + mOffset;
+    }
+
+    private void OnMouseUp()
+    {
+        block.selected = 0; 
+    }
+
+    private Vector3 GetMouseWorldPos() {
+        //pixel coordinates(x,y) 
+        Vector3 mousePoint = Input.mousePosition;
+
+        //z coordinate of game object on screen 
+        mousePoint.z = mZCoord;
+
+        return Camera.main.ScreenToWorldPoint(mousePoint); 
+    }
+}
